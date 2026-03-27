0c88c0467114857525df24835365a835094ae595
Anthony Nivault <a.familyanmi@outlook.com>
2026-03-27 11:07:19 +0100

    Création Page Erreur

--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
+
 function Erreur() {
-  return <h1>404 - Page introuvable</h1>
+  return (
+    <section className="error">
+      <h1 className="error__code">404</h1>
+      <p className="error__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </section>
+  )
 }
 
 export default Erreur
