6bc4e08673ad8fee0f3cbcdb0f82581438c8a461
Anthony Nivault <a.familyanmi@outlook.com>
2026-03-31 18:27:59 +0200

    Ajout du responsive mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import LogoFooter from '../assets/logos/logo2.svg'
+import LogoFooterMobile from '../assets/logos/logo3mobile.svg'
 
 function Footer() {
   return (
     <footer className="footer">
-      <img className="footer__logo" src={LogoFooter} alt="Logo Kasa © 2020 Kasa. All rights reserved" />
+      <picture>
+        <source media="(max-width: 768px)" srcSet={LogoFooterMobile} />
+        <img className="footer__logo" src={LogoFooter} alt="Logo Kasa © 2020 Kasa. All rights reserved" />
+      </picture>
     </footer>
   )
 }
